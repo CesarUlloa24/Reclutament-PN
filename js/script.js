3ec68a299e4ab0a31df197a2c7b60f6f42029d43
CesarUlloa24 <cesargaws@gmail.com>
2026-01-13 21:23:51 -0500

    Implementacion de la pagina 3 y arreglo del logo con opcion a subir foto

--- js/script.js
+++ js/script.js
@@ -121,7 +121,24 @@ function validarYPasar(n) {
     cambiarPagina(n);
 }
 
-// --- 6. FUNCIÓN FINALIZAR Y GUARDAR EN FIREBASE ---
+function previsualizarFoto(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function(e) {
+            const preview = document.getElementById('photo-preview');
+            // Reemplazamos el icono por la imagen real
+            preview.innerHTML = `<img src="${e.target.result}" alt="Foto Recluta">`;
+            
+            // Opcional: Si quieres guardar la imagen en el objeto 'datos' para mandarla al Excel (en formato Base64)
+            // Aunque recuerda que Excel tiene un límite de caracteres por celda.
+        }
+        
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+// --- 6. FUNCIÓN FINALIZAR 
 
 function finalizarFormulario() {
 
@@ -133,7 +150,7 @@ function finalizarFormulario() {
     if (btnFinalizar.disabled) return;
 
     console.log("Iniciando captura de datos...");
-    const urlScript = "https://script.google.com/macros/s/AKfycbz4M6SJdHYejVRJ0eBaARuheamzQZcC9dsE-OhGKlL91lN75rkKiILqhBuHQF3lzA8/exec"; 
+    const urlScript = "https://script.google.com/macros/s/AKfycbyAEdXOzvS7LhqRmWlFdKh7FjXOrHdX6utpMvPIdRPTFMO6mRFg0FySAW5vLL_n7TYz/exec"; 
 
     // 3. Deshabilitar el botón y mostrar estado de carga
     btnFinalizar.disabled = true;
@@ -233,7 +250,43 @@ function finalizarFormulario() {
 
         // SALUD PERSONAL (22 items)
         p1R: v('saludPers1'), p1E: v('expl-1'), p2R: v('saludPers2'), p2E: v('expl-2'), p3R: v('saludPers3'), p3E: v('expl-3'), p4R: v('saludPers4'), p4E: v('expl-4'), p5R: v('saludPers5'), p5E: v('expl-5'), p6R: v('saludPers6'), p6E: v('expl-6'), p7R: v('saludPers7'), p7E: v('expl-7'), p8R: v('saludPers8'), p8E: v('expl-8'), p9R: v('saludPers9'), p9E: v('expl-9'), p10R: v('saludPers10'), p10E: v('expl-10'),
-        p11R: v('saludPers11'), p11E: v('expl-11'), p12R: v('saludPers12'), p12E: v('expl-12'), p13R: v('saludPers13'), p13E: v('expl-13'), p14R: v('saludPers14'), p14E: v('expl-14'), p15R: v('saludPers15'), p15E: v('expl-15'), p16R: v('saludPers16'), p16E: v('expl-16'), p17R: v('saludPers17'), p17E: v('expl-17'), p18R: v('saludPers18'), p18E: v('expl-18'), p19R: v('saludPers19'), p19E: v('expl-19'), p20R: v('saludPers20'), p20E: v('expl-20'), p21R: v('saludPers21'), p21E: v('expl-21'), p22R: v('saludPers22'), p22E: v('expl-22')
+        p11R: v('saludPers11'), p11E: v('expl-11'), p12R: v('saludPers12'), p12E: v('expl-12'), p13R: v('saludPers13'), p13E: v('expl-13'), p14R: v('saludPers14'), p14E: v('expl-14'), p15R: v('saludPers15'), p15E: v('expl-15'), p16R: v('saludPers16'), p16E: v('expl-16'), p17R: v('saludPers17'), p17E: v('expl-17'), p18R: v('saludPers18'), p18E: v('expl-18'), p19R: v('saludPers19'), p19E: v('expl-19'), p20R: v('saludPers20'), p20E: v('expl-20'), p21R: v('saludPers21'), p21E: v('expl-21'), p22R: v('saludPers22'), p22E: v('expl-22'),
+
+        // PÁGINA 3
+          // SALUD PERSONAL (CONTINUACIÓN ITEMS 23 AL 31)
+        p23R: v('saludPers23'), p23E: v('expl-23'),
+        p24R: v('saludPers24'), p24E: v('expl-24'),
+        p25R: v('saludPers25'), p25E: v('expl-25'),
+        p26R: v('saludPers26'), p26E: v('expl-26'),
+        p27R: v('saludPers27'), p27E: v('expl-27'),
+        p28R: v('saludPers28'), p28E: v('expl-28'),
+        p29R: v('saludPers29'), p29E: v('expl-29'),
+        p30R: v('saludPers30'), p30E: v('expl-30'),
+        p31R: v('saludPers31'), p31E: v('expl-31'),
+
+        // XII.- HÁBITOS
+        habitosDrogas: v('habitosDrogas'),
+        habitosExpl: v('habitosExpl'),
+
+        // XIII.- ANTECEDENTES JUDICIALES
+        judicExpl: v('judicExpl'),
+
+        // XIV.- SITUACIÓN ACTUAL
+        actualLabora: v('actualLabora'),
+        actualEstudia: v('actualEstudia'),
+        actualPartido: v('actualPartido'),
+        actualEstamento: v('actualEstamento'),
+        actualExpl: v('actualExpl'),
+
+        // XV.- MOTIVACIONES
+        motivo1: v('motivo1'),
+        motivo2: v('motivo2'),
+        motivo3: v('motivo3'),
+        motivo4: v('motivo4'),
+        motivo5: v('motivo5'),
+        motivo6: v('motivo6'),
+        motivo7R: v('motivo7'),
+        motivo7E: v('expl-incidente')
     };
 
     console.log("Datos capturados listos para enviar:", datos);
