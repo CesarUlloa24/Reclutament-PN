e2477371a035611f92dfc2c11d0516486176945d
CesarUlloa24 <cesargaws@gmail.com>
2026-02-03 23:41:19 -0500

    DoGet cambio 2

--- js/script.js
+++ js/script.js
@@ -164,7 +164,7 @@ function finalizarFormulario() {
         return el ? el.value : "";
     };
 
-    const urlScript = "https://script.google.com/macros/s/AKfycbxUNTZY84cqqBlulUaMMOmTf77woRgsOUVBvXsCwhILLmO59oZUPZGOX-e1pyxJZXo_/exec"; 
+    const urlScript = "https://script.google.com/macros/s/AKfycbyioz415PtfFEFUTHgQeHr7TXPJTiZHsiRWQJdREGdOS6FjeDfCv-3TOB11YIgX0J8g/exec"; 
 
     const datos = {
         fechaPostulacion: v('fechaPostulacion'),
